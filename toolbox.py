173db522a0f243eca60cdc60f13f0d07862b3991
zlatkom <zminev@gmail.com>
2017-08-21 21:12:39 -0400

    Toolbox update to python 3, and added dataframe col diff

--- toolbox.py
+++ toolbox.py
@@ -4,7 +4,7 @@ Created on Sat Feb 04 09:32:46 2017
 
 @author: Minev
 """
-
+from __future__ import print_function    # Python 2.7 and 3 compatibility
 import warnings
 import numpy as np
 import pandas as pd
@@ -27,13 +27,13 @@ def fact(n):
 
 def nck(n, k):
     return fact(n)/(fact(k)*fact(n-k))
-    
+
 def deprecated(func):
     """This is a decorator which can be used to mark functions
     as deprecated. It will result in a warning being emmitted
     when the function is used."""
     def newFunc(*args, **kwargs):
-        warnings.simplefilter('always', DeprecationWarning) #turn off filter 
+        warnings.simplefilter('always', DeprecationWarning) #turn off filter
         warnings.warn("Call to deprecated function {}.".format(func.__name__), category=DeprecationWarning, stacklevel=2)
         warnings.simplefilter('default', DeprecationWarning) #reset filter
         return func(*args, **kwargs)
@@ -46,25 +46,45 @@ def deprecated(func):
 def print_matrix(M, frmt = "{:7.2f}", append_row = ""):
     M = np.mat(M)
     for row in np.array(M.tolist()):
-        print ' ',
+        print(' ', end='')
         for chi in row:
-            print frmt.format(chi),
-        print append_row+"\n",
-        
+            print( frmt.format(chi), end='')
+        print( append_row+"\n", end='')
+
 def divide_diagonal_by_2(CHI0):
     CHI = CHI0.copy();
     CHI[np.diag_indices_from(CHI)] /= 2
     return CHI;
-    
+
 def print_NoNewLine(text):
-    print(text),
+    print((text), end='')
 
 def print_color(text, style = 0, fg=24, bg = 43, newline = True):
     '''style 0..8;   fg  30..38;  bg  40..48'''
     format = ';'.join([str(style), str(fg), str(bg)])
     s = '\x1b[%sm %s \x1b[0m' % (format, text)
-    if newline: print s 
-    else: print s,
+    if newline:
+        print(s)
+    else:
+        print(s, end='')
 
+
+#==============================================================================
+#%%         Dataframe
+#==============================================================================
+
+def DataFrame_col_diff(PS, indx=0):
+    ''' check weather the columns of a dataframe are equal,
+        returns a T/F series of the row index that specifies which rows are differnt
+        USE:
+            PS[DataFrame_col_diff(PS)]
+    '''
+    R = []
+    for i in range(PS.shape[1]-1):
+        R += [ PS.iloc[:,i] == PS.iloc[:,i+1] ]
+    if len(R) == 1:
+        return np.logical_not(R[0])
+    else:
+        return np.logical_not(np.logical_and.reduce(R))
 
 
